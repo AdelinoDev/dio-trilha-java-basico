1988f9ec59e0a882f1c648c052ed04f78db75841
AdelinoNetto <adelino.mecanica@gmail.com>
2023-08-18 20:16:06 -0300

    repositorio tipos de variaveis atualizado

--- tipos-variaveis/TiposVariaveis.java
+++ tipos-variaveis/TiposVariaveis.java
@@ -2,6 +2,23 @@ package edu.adelino.primeirasemana;
 
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+        //TIPOS DE VARIAVEIS
+        //TIPOS INTEIROS
+
+        byte numero1 = 8;
+        short numero2 = 16;
+        int numero3 = 32;
+        long numero4 = 64l;
+
+        //TIPOS FLUTUANTES
+
+        float numero5 = 32.0f;
+        double numero6 = 64.0;
+        final double PI = 3.14; // constante
+
+        boolean numero7 = true;
+
+        String nome = "Adelino";
+        char letra = 'a';
     }
 }
