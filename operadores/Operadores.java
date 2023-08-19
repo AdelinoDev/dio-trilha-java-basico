387f04ad214dbac8e65c9835e4625fff06c35dcc
AdelinoNetto <adelino.mecanica@gmail.com>
2023-08-18 21:22:25 -0300

    criando repositorio operadores java basico

---
+++ operadores/Operadores.java
@@ -0,0 +1,44 @@
+package edu.adelino.primeirasemana;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        //OPERADORES ARITMETICOS
+        /*
+            +  => adição
+            -  => subtração
+            *  => multiplicação
+            /  => divisão
+            %  => modulo
+            ++ => incremento
+            -- => decremento
+         */
+
+        //OPERADORES RELACIONAIS
+        /*
+            ==  => igual a
+            !=  => diferente de
+            >   => maior que
+            <   => menor que
+            >=  => maior ou igual que
+            <=  => menor ou igual que
+         */
+
+        //OPERADORES LOGICOS
+        /*
+            &  => and (E)
+            |  => or (OU)
+            !  => not (negação)
+            ^  => xor
+            ||  => or curto circuito (OU)
+            &&  => and curto circuito (E)
+         */
+
+        //OPERADORES TERNARIO
+        /*
+            (condição) ? valor_se_verdadeiro : valor_se_falso
+            Ex.:
+            (2 > 4 ) ? 50 : 80  -> resposta: 80
+         */
+    }
+}
